b0511e1a487c4435486173e54e01b558642a94a5
sabihDev <8203sabi.rahman@gmail.com>
2024-09-08 18:08:52 +0500

    Sentry setuped

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//...
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://9072217c9eeda78e3a4835a27d750e82@o4507917230473216.ingest.us.sentry.io/4507917236764672",
   integrations: [
     Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
   ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracesSampleRate: 1.0, 
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
 });
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
